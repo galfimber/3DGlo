195ce6c392fad29ddd5de984add6dba7c2f97077
Nikita Varankin <galfimber2014@yandex.ru>
2023-12-30 16:19:15 +0500

    Выполнено задание 23

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import popup from "./modules/popup";
 import scroll from "./modules/scroll";
 import reg from "./modules/reg";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
 timer("25 december 2023");
 menu();
@@ -11,3 +12,4 @@ popup();
 scroll();
 reg();
 tabs();
+slider();
