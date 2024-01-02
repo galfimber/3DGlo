ffd010bb5cccd0cdf4e327be83bbacf804049f19
Nikita Varankin <galfimber2014@yandex.ru>
2024-01-02 16:04:02 +0500

    Выполнено задание 25

---
+++ src/modules/helpers.js
@@ -0,0 +1,33 @@
+const animate = (func, popup, count) => {
+  console.log(count);
+
+  const popupShow = () => {
+    count += 0.1;
+    const idIntterval = requestAnimationFrame(popupShow);
+    if (count <= 1) {
+      popup.style.opacity = count;
+    } else {
+      cancelAnimationFrame(idIntterval);
+    }
+  };
+
+  const popupHide = () => {
+    count -= 0.1;
+    const idIntterval = requestAnimationFrame(popupHide);
+    if (count >= 0) {
+      popup.style.opacity = count;
+    } else {
+      cancelAnimationFrame(idIntterval);
+      popup.style.opacity = 0;
+      popup.style.display = "none";
+    }
+  };
+
+  if (func === "popupShow") {
+    popupShow();
+  } else {
+    popupHide();
+  }
+};
+
+export { animate };
--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,41 +1,44 @@
+import { animate } from "./helpers";
+
 const popup = () => {
   const popup = document.querySelector(".popup");
   const popupContent = document.querySelector(".popup-content");
   const popupBtn = document.querySelectorAll(".popup-btn");
   const popupClose = document.querySelector(".popup-close");
 
-  let idIntterval;
-  let count = -50;
+  //let idIntterval;
+  // let count = 0;
 
-  popupContent.style.top = `${count}%`;
+  //popupContent.style.top = `${count}%`;
 
-  const popupShow = () => {
-    count += 5;
-    idIntterval = requestAnimationFrame(popupShow);
-    if (count <= 10) {
-      popupContent.style.top = `${count}%`;
-    } else {
-      cancelAnimationFrame(idIntterval);
-    }
-  };
-  const popupHide = () => {
-    count -= 5;
-    idIntterval = requestAnimationFrame(popupHide);
-    if (count >= -50) {
-      popupContent.style.top = `${count}%`;
-    } else {
-      cancelAnimationFrame(idIntterval);
-      popup.style.display = "none";
-    }
-  };
+  // const popupShow = () => {
+  //   count += 5;
+  //   idIntterval = requestAnimationFrame(popupShow);
+  //   if (count <= 10) {
+  //     popupContent.style.top = `${count}%`;
+  //   } else {
+  //     cancelAnimationFrame(idIntterval);
+  //   }
+  // };
+  // const popupHide = () => {
+  //   count -= 5;
+  //   idIntterval = requestAnimationFrame(popupHide);
+  //   if (count >= -50) {
+  //     popupContent.style.top = `${count}%`;
+  //   } else {
+  //     cancelAnimationFrame(idIntterval);
+  //     popup.style.display = "none";
+  //   }
+  // };
 
   popupBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
       popup.style.display = "block";
       if (document.documentElement.clientHeight > 768) {
-        popupShow();
+        //popupShow();
+        animate("popupShow", popup, 0);
       } else {
-        popupContent.style.top = `10%`;
+        popup.style.opacity = `1`;
       }
     });
   });
@@ -46,7 +49,8 @@ const popup = () => {
       e.target.classList.contains("popup-close")
     ) {
       if (document.documentElement.clientHeight > 768) {
-        popupHide();
+        //popupHide();
+        animate("popupHide", popup, 1);
       } else {
         popup.style.display = "none";
       }
